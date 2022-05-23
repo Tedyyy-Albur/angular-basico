09723391eb2411919a933a18127ba2e2a10f267a
Arturo Alburquerque <alburquerquerangelarturo@gmail.com>
2022-05-23 14:00:19 -0500

    commit

--- src/app/heroes/heroe.component.ts
+++ src/app/heroes/heroe.component.ts
@@ -3,10 +3,17 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-heroe',
-    templateUrl: 'heroe.component.html'
+    templateUrl: './heroe.component.html'
 })
 
 export class HeroeComponent{
     nombre: string = 'ironman';
     edad: number = 45
+
+    obtenerNombre(): string{
+        return `${this.nombre} - ${this.edad} `;
+    }
+
+
+
 }
